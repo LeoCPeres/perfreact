cbe5d5ac5b7e46d1d7de681df254d139f34cde7e
Leonardo Csik Peres <leonardocsikperes@gmail.com>
2022-07-03 15:15:00 -0300

    Memo

--- src/components/ProductItem.tsx
+++ src/components/ProductItem.tsx
@@ -1,3 +1,5 @@
+import { memo } from "react";
+
 type ProductItemProps = {
   product: {
     id: number;
@@ -6,10 +8,24 @@ type ProductItemProps = {
   };
 };
 
-export function ProductItem({ product }: ProductItemProps) {
+function ProductItemComponent({ product }: ProductItemProps) {
   return (
     <div>
-      {product.title} - <strong>{product.title}</strong>
+      {product.title} - <strong>{product.price}</strong>
     </div>
   );
 }
+
+export const ProductItem = memo(
+  ProductItemComponent,
+  (prevProps, nextProps) => {
+    return Object.is(prevProps.product, nextProps.product); //comparação profunda (custa mais processamento)
+  }
+);
+
+//o memo vai fazer com que o componente seja reutilizado, ou seja, se o componente não tiver mudado,
+//ele não será renderizado novamente
+//https://pt-br.reactjs.org/docs/react-api.html#reactmemo
+
+//shallow compare -> comparação superficial, ou seja, só compara o que tem dentro do componente
+//tipo {} === {}
