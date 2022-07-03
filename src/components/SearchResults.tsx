e0350b3fb8147448c6f94fb1e5a10deae17c0c9f
Leonardo Csik Peres <leonardocsikperes@gmail.com>
2022-07-03 15:26:57 -0300

    useMemo

--- src/components/SearchResults.tsx
+++ src/components/SearchResults.tsx
@@ -1,15 +1,32 @@
 import { ProductItem } from "./ProductItem";
+import { useMemo } from "react";
 
 type SearchResultsProps = {
   results: Array<{ id: number; price: number; title: string }>;
 };
 
 export function SearchResults({ results }: SearchResultsProps) {
+  const totalPrice = useMemo(() => {
+    return results.reduce((total, product) => {
+      return total + product.price;
+    }, 0);
+  }, [results]);
+
   return (
     <div>
+      <h2>{totalPrice}</h2>
+
       {results.map((product) => {
         return <ProductItem product={product} />;
       })}
     </div>
   );
 }
+
+// o useMemo é um hook que permite que você memoize um valor, ou seja,
+// ele só calcula o valor quando o valor é diferente do valor anterior
+// https://pt-br.reactjs.org/docs/hooks-reference.html#usememo
+
+// não utilizar useMemo em cálculos "simples".
+
+// podemos utilizar o useMemo para repassar uma info a um componente filho
